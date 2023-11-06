cd4c1cdb08331c59434a7b1f05eaeaf6c100d344
Carlos Matos <carlinhos12345678962@gmail.com>
2023-11-06 16:45:36 -0300

    ListAPIView e Autenticação

--- setup/urls.py
+++ setup/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-from escola.views import AlunosViewSet, CursosViewSet, MatriculasViewSet
+from django.urls import path, include
+from escola.views import AlunosViewSet, CursosViewSet, MatriculasViewSet, ListaMatraticulasAluno, ListaAlunosMatriculados
 from rest_framework import routers
 
 router = routers.DefaultRouter()
@@ -26,5 +26,7 @@ router.register('matriculas', MatriculasViewSet, basename='Matriculas')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls) )
+    path('', include(router.urls)),
+    path("aluno/<int:pk>/matriculas/", ListaMatraticulasAluno.as_view()),
+    path("curso/<int:pk>/matriculas", ListaAlunosMatriculados.as_view()),
 ]
